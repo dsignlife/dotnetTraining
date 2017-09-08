57a03198927327ba6778be9ef7b007171cce60f2
Tomz Phoz <dsign-life@hotmail.com>
2017-09-08 22:21:05 +0200

    simple backend

---
+++ BCCCBackend/BCCCBackend/Prop.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BCCCBackend
+{
+    class Prop
+    {
+        public string Name { get; set; }
+        public double Rate { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public double Ask { get; set; }
+        public double Bid { get; set; }
+    }
+}
