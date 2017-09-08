9a1626502fc68f63fe52d3d0295ba1bd31b1368f
Tomz Phoz <dsign-life@hotmail.com>
2017-09-08 11:25:19 +0200

    Simple aspnet project added

---
+++ Codity/L2Array/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L2Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = {9,3,9,3,9,7,9 };
+            Console.WriteLine(solution(input));
+            
+        }
+
+        public static int solution(int[] A)
+        {
+
+           // Array.Sort(A);
+            int count = 0;
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                count = count ^ A[i];
+                 
+            }
+
+
+
+            return count;
+        }
+
+
+    }
+}
