c1550a066c749230ffdba64c1d2e5e4c3293a684
Tomz Phoz <dsign-life@hotmail.com>
2017-09-09 21:08:20 +0200

    add sql query for btc table

---
+++ BTCChart/BCCCBackend/BCCCBackend/sql/dbo.BTC.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[BTC] (
+    [Name] NCHAR (20)   NULL,
+    [Rate] DECIMAL (18) NULL,
+    [Date] NCHAR (15)   NULL,
+    [Time] NCHAR (10)   NULL,
+    [Ask]  DECIMAL (18) NULL,
+    [Bid]  DECIMAL (18) NULL
+);
+
