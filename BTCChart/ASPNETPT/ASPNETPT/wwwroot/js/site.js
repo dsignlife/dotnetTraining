41af011539f9cdf98918adcf6c0abbf069371b0f
Tomz Phoz <dsign-life@hotmail.com>
2017-09-11 20:35:56 +0200

    add some bootstrap

--- BTCChart/ASPNETPT/ASPNETPT/wwwroot/js/site.js
+++ BTCChart/ASPNETPT/ASPNETPT/wwwroot/js/site.js
@@ -1,39 +1,19 @@
 ﻿
 (function () { 
 
-    
-
-
-    //var ele = $("#username");
-    //ele.text = "Tomz";
-
-    //var main = $("#main");
-    //main.on("mouseenter", function() {
-
-    //    main.css("background-color", "#888");
-    //});
-
-    //main.on("mouseleave", function() {
-    //    main.css("background-color", "");
-    //});
-
-    //var menuItems = $("ul.menu li a");
-    //menuItems.on("click",
-    //    function () {
-    //        var me = $(this);
-    //        alert(me.text());
-    //    });
-
 
     var $sidebarAndWrapper = $("#sidebar, #wrapper");
+    var $icon = $("#sToggle i.fa");
 
     $("#sToggle").on("click", function () {
 
         $sidebarAndWrapper.toggleClass("hide-sidebar");
         if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
-            $(this).text("Show Sidebar");
+            $icon.removeClass("fa-angle-left");
+             $icon.addClass("fa-angle-right");
         } else {
-            $(this).text("Hide Sidebar");
+             $icon.addClass("fa-angle-left");
+             $icon.removeClass("fa-angle-right");
         }
     });
 
