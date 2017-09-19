269ce6137f4b39eaca6db155037d595e8ca5a215
Tomz Phoz <dsign-life@hotmail.com>
2017-09-19 10:06:39 +0200

    simple bandwithchecker

---
+++ CodePlayground/BandwidthChecker/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace BandwidthChecker
+{
+    class Program
+    {
+        private const double timerUpdate = 1000;
+        static int bytesSentSpeed = 0;
+        static int bytesReceivedSpeed = 0;
+
+        static void Main(string[] args)
+        {
+
+            int choice = 0;
+
+            while (choice == 0)
+            {
+
+
+                if (choice == 0)
+                {
+                    var timer = new System.Threading.Timer(e => BandwidthChecker(), null, TimeSpan.Zero, TimeSpan.FromSeconds(1));
+                    
+                    choice = Convert.ToInt32(ReadLine());
+                    
+                }
+
+
+                else
+                {
+                    Environment.Exit(0);
+                }
+
+            }
+
+        }
+
+        public static void BandwidthChecker()
+        {
+
+            IPv4InterfaceStatistics interfaceStats = NetworkInterface.GetAllNetworkInterfaces()[0].GetIPv4Statistics();
+            bytesSentSpeed = (int)(interfaceStats.BytesSent - bytesSentSpeed) / 1024;
+            bytesReceivedSpeed = (int)(interfaceStats.BytesReceived - bytesReceivedSpeed) / 1024;
+
+            WriteLine($"Download: {bytesReceivedSpeed}\nUpload: {bytesSentSpeed}");
+            SetCursorPosition(0, CursorTop -2);
+            Read();
+
+
+        }
+
+
+    }
+}
